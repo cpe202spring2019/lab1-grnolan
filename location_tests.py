3a86abee9cad05c7f5a72fedc7fe115de54b2d08
grnolan <grnolan@unix1.csc.calpoly.edu>
2019-04-09 17:21:09 -0700

    tests added

--- location_tests.py
+++ location_tests.py
@@ -4,10 +4,26 @@ from location import *
 class TestLab1(unittest.TestCase):
 
     def test_repr(self):
-        loc = Location("SLO", 35.3, -120.7)
-        self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location("Paris", 48.9, 2.4)
+        loc3 = Location("SLO", 35.3, -120.7)
+        loc4 = loc1
+        self.assertEqual(repr(loc1),"Location('SLO', 35.3, -120.7)")
+        self.assertEqual(repr(loc2),"Location('Paris', 48.9, 2.4)")
+        self.assertEqual(repr(loc3),"Location('SLO', 35.3, -120.7)")
+        self.assertEqual(repr(loc4),"Location('SLO', 35.3, -120.7)")
+
     # Add more tests!
+
+    def test_eq(self):
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location("Paris", 48.9, 2.4)
+        loc3 = Location("SLO", 35.3, -120.7)
+        loc4 = loc1
+        self.assertEqual(Location.__eq__(loc1, loc3), True)
+        self.assertEqual(Location.__eq__(loc1, loc4), True)
+        self.assertEqual(Location.__eq__(loc1, loc2), False)
+        self.assertEqual(Location.__eq__(loc4, loc2), False)
 
 if __name__ == "__main__":
         unittest.main()
